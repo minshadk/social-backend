0e160c37e07a8632054bf9bf21d27ca621dcba8f
minshadk <minshadk13@gmail.com>
2022-02-09 23:18:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+
+dotenv.config();
+
+// mongoose.connect(
+//   process.env.MONGO_URL,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   }, 
+//   () => {
+//     console.log("Connected to MongoDB");
+//   } 
+// );
+console.log(process.env.MONGO_URL)
+mongoose 
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("connected to MOngodb"))
+  .catch((err) => console.error(err));
+
+// middleware 
+app.use(express.json())
+app.use(helmet()); 
+app.use(morgan("common"))
+
+app.use("/api/users",userRoute)
+app.use("/api/auths",authRoute)
+
+app.listen(8800, () => { 
+  console.log("Backend is running");
+});
+  
