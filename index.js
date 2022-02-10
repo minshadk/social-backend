f002dd15868e9d767706b96fc713f83fd7b86627
minshadk <minshadk13@gmail.com>
2022-02-10 23:29:22 +0530

    user, post rc completed

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
-const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
+const postRoute = require("./routes/posts")
 
 dotenv.config();
 
@@ -33,6 +34,7 @@ app.use(morgan("common"))
 
 app.use("/api/users",userRoute)
 app.use("/api/auths",authRoute)
+app.use("/api/posts",postRoute)
 
 app.listen(8800, () => { 
   console.log("Backend is running");
